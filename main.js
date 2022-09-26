af256a1e95870b95949ba19034ef252446140175
bcmfranco <bcmfranco@gmail.com>
2022-09-26 12:45:55 -0300

    Add all

---
+++ main.js
@@ -0,0 +1,44 @@
+// Vars
+// Number generator
+var figures = 2; // Cantidad de cifras del número
+var operator = "+"; // Operador
+var first = Math.round(Math.random()*100, 0); // Número first del target
+var second = Math.round(Math.random()*100, 0); // Número second del target
+var tri = 0;
+
+// Functions
+function calc(first, second, tri){
+    
+    var real_result = first+second;
+
+    if(tri == ''){
+        tri = 0;
+    }
+
+    var tried_result = tri;
+
+    if(real_result != tried_result){
+        var response = false;
+    } else {
+        var response = true;
+    }
+
+    return response;
+}
+
+// Course
+// Render
+$$('#first').set('text', first);
+$$('#second').set('text', second);
+
+$$('#send').addEvent('click', function(event){
+    
+    var tri = $$('input[name=diana_input]').get('value')[0];
+
+    var resulting = calc(first, second, tri);
+
+    console.log(resulting);
+
+
+})
+
