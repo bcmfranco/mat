71fd106d08632a4b9cccbda164c8b7b25bfcbf4e
bcmfranco <bcmfranco@gmail.com>
2022-09-26 14:29:43 -0300

    Add time_barr

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ var second = Math.round(Math.random()*100, 0); // Número second del target
 var tri = 0;
 
 // Functions
-function calc(first, second, tri){
-    
+function calc(first, second, tri){ // Response si el resultado enviado por el usuario es correcto
     var real_result = first+second;
 
     if(tri == ''){
@@ -29,19 +28,44 @@ function calc(first, second, tri){
     return response;
 }
 
+function game_over(){
+    $$('#send').set('text', '');
+    $$('#response').set('text', 'TIME OUT');
+}
+
+function start(){
+
+    $$('#first').set('text', first);
+    $$('#second').set('text', second);
+
+    var starting_counter = 5;
+    var interval = setInterval(function() {
+        starting_counter--;
+
+        var percentage = 100/5*starting_counter;
+        $$('#time_barr').setStyle('width', percentage+'%');
+
+        if (starting_counter == 0) {
+            clearInterval(interval);
+            game_over();
+        }
+    
+        return starting_counter;
+    
+    }, 1000);
+}
+//////
+
 // Course
 // Render
-$$('#first').set('text', first);
-$$('#second').set('text', second);
+$$('#start').addEvent('click', function(event){
+    start();
+});
 
 $$('#send').addEvent('click', function(event){
-    
     var tri = $$('input[name=diana_input]').get('value')[0];
-
     var resulting = calc(first, second, tri);
+});
 
-    console.log(resulting);
-
-
-})
-
+// Hay que hacerle una función de stop para que deje de bajar la barra
+// y muestre el resultado por encima del time out
